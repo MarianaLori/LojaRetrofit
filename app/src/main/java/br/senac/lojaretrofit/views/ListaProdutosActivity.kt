5fb4d0f7dca2f41a2956f08162466c9c24c0a188
Mariana Lori <mariana.lori@hotmail.com>
2022-05-24 22:44:50 -0300

    Loja usando Retrofit

--- app/src/main/java/br/senac/lojaretrofit/views/ListaProdutosActivity.kt
+++ app/src/main/java/br/senac/lojaretrofit/views/ListaProdutosActivity.kt
@@ -1,8 +1,19 @@
 package br.senac.lojaretrofit.views
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import br.senac.lojaretrofit.databinding.ActivityListaProdutosBinding
+import br.senac.lojaretrofit.databinding.CardItemBinding
+import br.senac.lojaretrofit.model.Produto
+import br.senac.lojaretrofit.services.API
+import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
+import retrofit2.Call
+import retrofit2.Response
+import java.net.CacheResponse
+import javax.security.auth.callback.Callback
 
 class ListaProdutosActivity : AppCompatActivity() {
     lateinit var binding: ActivityListaProdutosBinding
@@ -11,6 +22,53 @@ class ListaProdutosActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityListaProdutosBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        listarProdutos()
+    }
+
+    fun listarProdutos() {
+        val callback = object : retrofit2.Callback<List<Produto>>{
+            override fun onResponse(call: Call<List<Produto>>, response: Response<List<Produto>>) {
+                if (response.isSuccessful){
+                    val ListaProdutos = response.body()
+                    atualizarTela(ListaProdutos)
+                }
+
+                binding.progressBar.visibility = View.INVISIBLE
+            }
+
+            override fun onFailure(call: Call<List<Produto>>, t: Throwable) {
+                Snackbar.make(binding.container,
+                "Não",
+                    Snackbar.LENGTH_LONG)
+                    .show()
+                binding.progressBar.visibility = View.INVISIBLE
+            }
+
+        }
+
+        API.produto.listar().enqueue(callback)
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    fun atualizarTela (listaProdutos: List<Produto>?) {
+        binding.container.removeAllViews()
+
+        listaProdutos?.forEach {
+            val cardBinding = CardItemBinding.inflate(layoutInflater)
+
+            cardBinding.textNome.text = it.nomeProduto
+            cardBinding.textNome.text = it.precProduto.toString()
+
+            Picasso
+                .get()
+                .load("https://oficinacordova.azurewebsites.net/android/rest/produto/image/${it.idProduto}")
+                .into(cardBinding.image)
+
+
+            binding.container.addView(cardBinding.root)
+
+        }
     }
 
 }
